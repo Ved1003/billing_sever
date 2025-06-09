c0346e107ba25949688dfd01c15622b3080f2e57
Ved Brahmankar <96011530+Ved1003@users.noreply.github.com>
2025-06-09 10:03:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const connectDB = require('./config/db');
-// const errorHandler = require('./middlewares/errorHandler');
-
-// // Connect to MongoDB
-// connectDB();
-
-// const app = express();
-// app.use(express.json());
-
-// // API routes
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/customers', require('./routes/customers'));
-// app.use('/api/categories', require('./routes/categories'));
-// app.use('/api/products', require('./routes/products'));
-// app.use('/api/invoices', require('./routes/invoices'));
-
-// // Global error handler
-// app.use(errorHandler);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
@@ -57,3 +31,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
